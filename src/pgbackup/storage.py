27686a429ce0b277900aeddad49fd305ff16ad3a
Jason Gardner <zx9hippo@yahoo.com>
2020-01-13 02:09:22 +0000

    storage updated and tested

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -1,6 +1,9 @@
+
+
 def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
 
-
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
